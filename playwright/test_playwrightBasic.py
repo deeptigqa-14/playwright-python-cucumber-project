d36120ffc6c20f89bb828950f2c09fa1ca203e24
Deepti G <deeptigqa@gmail.com>
2026-03-23 14:24:17 -0500

    Launch different browser in playwright and use locator

--- playwright/test_playwrightBasic.py
+++ playwright/test_playwrightBasic.py
@@ -1,4 +1,5 @@
-from playwright.sync_api import Page #
+import time
+from playwright.sync_api import Page, expect, Playwright  #
 
 def test_playwrightBasics(playwright): # globally available feature which we can use in our test cases need to pass a argument to use it
     # create a browser instance
@@ -11,3 +12,35 @@ def test_playwrightBasics(playwright): # globally available feature which we can
 #page fixture coming from Page class , if you want to all the methods of Page class you need to import it and use it as a argument in the test case, it will automatically create a page instance for you and you can use it in your test case
 def test_playwrightShortcut(page:Page):
     page.goto("https://rahulshettyacademy.com/")
+
+
+# css locator present with "#id" or ".class" or "tagname[attribute='value']"
+def test_corelocators(page:Page):
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    #page.get_by_role("button", name="Sign in").click()
+    expect(page.get_by_text("Empty username/password.")).to_be_visible()
+
+    page.get_by_label("UserName").fill("rahulshettyacademy") # should have a label tag. input tag should be wrapped between the label tag
+    page.get_by_label("Password").fill("learning")
+    page.get_by_role("combobox").select_option("teach")
+    page.locator("#terms").check()
+    page.get_by_role("link", name="Terms and Conditions").click()
+    time.sleep(5)
+    page.get_by_role("button", name="Sign in").click()
+
+def test_firefoxBrowser(playwright:Playwright):
+    firfoxbrowser = playwright.firefox.launch(headless=False)
+    context = firfoxbrowser.new_context()
+    page = context.new_page()
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    page.get_by_role("button", name="Sign in").click()
+    expect(page.get_by_text("Empty username/password.")).to_be_visible()
+
+    page.get_by_label("UserName").fill(
+        "rahulshettyacademy")  # should have a label tag. input tag should be wrapped between the label tag
+    page.get_by_label("Password").fill("learning")
+    page.get_by_role("combobox").select_option("teach")
+    page.locator("#terms").check()
+    page.get_by_role("link", name="Terms and Conditions").click()
+    time.sleep(5)
+    page.get_by_role("button", name="Sign in").click()
